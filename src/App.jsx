39257703d63d9e626f9ac678cc9841c75d6e1010
Ganesh Bhandari <gb2544779@gmail.com>
2026-08-04 23:35:29 +0545

    Fix file names

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import AppRoutes from './routes/AppRoutes'
-import ScrollToTop from './components/common/ScrollToTop'
+import ScrollToTop from './components/common/ScrollTOTop.jsx'
 
 function App() {
   return (
