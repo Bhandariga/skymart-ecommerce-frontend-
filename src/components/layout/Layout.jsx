4ade3793bd956223239ac3cfee38bad8720acded
Ganesh Bhandari <gb2544779@gmail.com>
2026-08-05 17:40:21 +0545

    Fix routing and layout structure

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,18 +1,13 @@
-// src/components/layout/Layout.jsx
-import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
-import Footer from './Footer'
-
-const Layout = () => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  )
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+function Layout(){
+return(
+<div className="min-h-screen bg-white dark:bg-[#070A0F]">
+<Navbar/>
+<Outlet/>
+<Footer/>
+</div>
+)
 }
-
-export default Layout
+export default Layout;
