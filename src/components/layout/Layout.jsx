6ea23d5d165f66e17a7164d2f82f6f06de101903
Ganesh Bhandari <gb2544779@gmail.com>
2026-08-05 17:46:52 +0545

    Update Layout theme support

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,33 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
 function Layout(){
+
 return(
-<div className="min-h-screen bg-white dark:bg-[#070A0F]">
+
+<div className="
+min-h-screen
+bg-white
+dark:bg-[#070A0F]
+text-gray-900
+dark:text-white
+transition-colors
+duration-300
+">
+
 <Navbar/>
+
+<main>
 <Outlet/>
+</main>
+
 <Footer/>
+
 </div>
+
 )
+
 }
 
 export default Layout;
